c0f25ed3f08b1673d0d008dee07e09df5bb011f6
A2GMer <52153162+A2GMer@users.noreply.github.com>
2023-04-23 16:11:03 +0900

    clone blocks automatically

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject blockPrefab; // ブロックのPrefab
+    public int rows = 5; // ブロックの行数
+    public int cols = 10; // ブロックの列数
+    public Transform blockMap; // ブロックの親オブジェクト
+
+    void Start()
+    {
+        // ブロックを自動生成する
+        GenerateBlocks();
+    }
+
     public void OnClickToPlaySceneButton()
     {
         SceneManager.LoadScene("Play");
+    }
+
+    // ブロックを自動生成するメソッド
+    void GenerateBlocks()
+    {
+        // ブロックの間隔を設定する
+        float spacingX = 1f;
+        float spacingY = 0.5f;
+
+        // ブロックを生成する
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // ブロックの位置を計算する
+                float posX = j * spacingX - ((cols - 1) * spacingX) / 2;
+                float posY = i * spacingY + 3f;
+
+                // ブロックを生成する
+                GameObject block = Instantiate(blockPrefab, new Vector3(posX, posY, 0f), Quaternion.identity);
+                block.transform.parent = transform;
+            }
+        }
     }
 }
